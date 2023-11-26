6ace1b92b05e760ef695c98c01560f76a47fd632
nataliravich <ravich.natali@gmail.com>
2023-11-26 16:45:08 +0200

    replacing one number with another number

---
+++ 8kyu/replace-any-digit-Fake-Binary.js
@@ -0,0 +1,16 @@
+// DESCRIPTION:
+// Given a string of digits, you should replace any digit below 5 with '0' 
+// and any digit 5 and above with '1'. Return the resulting string.
+// Note: input will never be an empty string
+
+// Solution:
+function fakeBin(x) {
+	let res = "";
+	for (let i = 0; i < x.length; i += 1) {
+		if (x[i] < 5) {
+			res += 0;
+		} else {
+			res += 1;
+		}
+	} return res;
+}
