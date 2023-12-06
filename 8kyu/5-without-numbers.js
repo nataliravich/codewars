e0a1cba212cc66c69d35319ce825749474eb5ace
nataliravich <ravich.natali@gmail.com>
2023-12-06 22:53:05 +0200

    function always returns 5

---
+++ 8kyu/5-without-numbers.js
@@ -0,0 +1,17 @@
+/* Write a function that always returns 5
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/ */
+
+function unusualFive() {
+	let res = 'Five!';
+	return res.length;
+}
+
+function unusualFive() {
+	let res = ['one', 'two', 'three', 'four', 'five'];
+	return res.length;
+}
+
+function unusualFive() {
+	return ['one', 'two', 'three', 'four', 'five'].length;
+}
+
